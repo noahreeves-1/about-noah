8aec0601d40ba65e51db4d7531091a32a4561da3
Noah Kim <heanokim@gmail.com>
2021-09-29 11:54:24 -0700

    general updates

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Files for my about website.
 
 Today's date is September 10, 2021
+
+9/28/21 - pancake photo by Alesia Kozik from Pexels.com // https://www.pexels.com/@alesiakozik
+
--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("main").style.marginRight = "0px";
     document.body.style.backgroundColor = "#031532";
 }
