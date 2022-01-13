a951519ecb2d38492536f5d6a81392552368578b
Noah Kim <heanokim@gmail.com>
2022-01-12 23:14:32 -0800

    Added brand logos and some animation.

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "180px";
-    document.getElementById("main").style.marginLeft = "180px";
-    document.body.style.backgroundColor = "#031532";
-}
+const brands = document.querySelectorAll('.brand');
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0px";
-    document.getElementById("main").style.marginRight = "0px";
-    document.body.style.backgroundColor = "#031532";
-}
+brands.forEach((brand) => {
+    brand.addEventListener('mouseover', e => {
+        brand.classList.remove('grayscale');
+    })
+
+    brand.addEventListener('mouseout', e => {
+        brand.classList.add('grayscale');
+    })
+})
